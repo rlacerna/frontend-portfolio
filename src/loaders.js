780d55a30af92be9df6b5c10b85111ce5bb726c6
Renz Lacerna <renzlacerna13@gmail.com>
2023-10-17 20:25:35 -0400

    Deploying

--- src/loaders.js
+++ src/loaders.js
@@ -1,6 +1,6 @@
 // BASE URL OF OUR DEPLOYED API
 // BASE URL OF OUR DEPLOYED API
-const URL = "http://localhost:4000"
+const URL = "https://backend-portfolio-9w4n.onrender.com"
 
 export const projectsLoader = async () => {
     const response = await fetch(URL + "/projects")
